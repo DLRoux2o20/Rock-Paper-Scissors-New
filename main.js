c24daf75de4256bba18aedd822be90a5f26a7796
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2024-12-15 16:23:56 +0200

    Make every console thing into the DOM. Play five rounds and can play again by pressing play agian button

--- main.js
+++ main.js
@@ -5,99 +5,153 @@ function defineA() {
 let a = defineA();
 let computerScore = 0;
 let playerScore = 0;
-let choiceInvalidDetector = 0;
+let playerScreenScore = document.getElementById("pScoreCounter");
+let computerScreenScore = document.getElementById("cScoreCounter");
+let roundResults = document.getElementById("roundResults");
+
+let btnRock = document.getElementById("btn1");
+let btnPaper = document.getElementById("btn2");
+let btnScissors = document.getElementById("btn3");
+let choice1 = document.getElementById("choice1");
+let choice2 = document.getElementById("choice2");
 
 function getComputerChoice() {
 
     if (a === 0) {
+        choice2.textContent = "Rock";
         return "Rock";
     } else if (a === 1) {
+        choice2.textContent = "Paper";
         return "Paper";
     }
+    choice2.textContent = "Scissors";
     return "Scissors"; 
 }
 
-let computerChoice = getComputerChoice();
+let computerChoice = "";
+let playerSelection = "";
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-         playRound();
-        function playRound(playerSelection, computerSelection) {
+    
+        function playRound() {
+        
     computerChoice = getComputerChoice();
-    let playerText = prompt("What is your choice?");
 
-    if (playerText) {
-        let playerSelection = playerText.toLowerCase();
-        let playerSelectionCapital = (playerSelection.charAt(0)).toUpperCase() + playerSelection.substr(1, (playerSelection.length) - 1);
-        alert(computerChoice + " " + "vs" + " " + playerSelectionCapital);
-
-        console.log("Computer Choice: " + computerChoice);
-        console.log("Your Choice: " + playerSelectionCapital);
-
-        if (playerSelection === "rock") {
+        if (playerSelection === "Rock") {
             var b = 0;
-        } else if (playerSelection === "paper") {
+        } else if (playerSelection === "Paper") {
             var b = 1;
-        } else if (playerSelection === "scissors") {
+        } else if (playerSelection === "Scissors") {
             var b = 2;
-        } else {
-            alert("Choice invalid");
-            i--;
-            choiceInvalidDetector = 1;
         }
 
-        if (choiceInvalidDetector === 1) {
-            console.log("Choice Invalid");
-            console.log(" ");
-            choiceInvalidDetector = 0;
-            a = defineA();
-        } else if (a === 0 && b === 2) {
-            console.log(`You lose! Rock beats Scissors`);
+        if (a === 0 && b === 2) {
+            roundResults.textContent = "You lose! Rock beats Scissors";
             computerScore += 1;
-            console.log("Your Score: " + playerScore);
-            console.log("Computer Score: " + computerScore);
-            console.log(" ");
+            computerScreenScore.textContent = `${computerScore}`;
             a = defineA();
         } else if (a === 2 && b === 0) {
-            console.log("You won! Rock beats Scissors");
+            roundResults.textContent = "You won! Rock beats Scissors";
             playerScore += 1;
-            console.log("Your Score: " + playerScore);
-            console.log("Computer Score: " + computerScore);
-            console.log(" ");
+            playerScreenScore.textContent = `${playerScore}`;
             a = defineA();
         } else if (a > b) {
-            console.log(`You lost! ${computerChoice} beats ${playerSelectionCapital}`);
+            roundResults.textContent = `You lost! ${computerChoice} beats ${playerSelection}`;
             computerScore += 1;
-            console.log("Your Score: " + playerScore);
-            console.log("Computer Score: " + computerScore);
-            console.log(" ");
+            computerScreenScore.textContent = `${computerScore}`;
             a = defineA();
         } else if (a < b) {
-            console.log(`You won! ${playerSelectionCapital} beats ${computerChoice}`);
+            roundResults.textContent = `You won! ${playerSelection} beats ${computerChoice}`;
             playerScore += 1;
-            console.log("Your Score: " + playerScore);
-            console.log("Computer Score: " + computerScore);
-            console.log(" ");
+            playerScreenScore.textContent = `${playerScore}`;
             a = defineA();
         } else if (a === b) {
-            console.log("It was a tie");
+            roundResults.textContent = "It was a tie";
             a = defineA();
         }
 
-    } else {
-        alert("Please enter choice");
-    }
-    }
-}
+    if (playerScore === 5) {
+        let endScreen = document.createElement("div");
+        document.body.appendChild(endScreen);
+        endScreen.style.cssText = "position: absolute; top: 0; width: 100vw; height: 100vh;";
+        endScreen.style.backgroundColor = "black";
+        endScreen.style.zIndex = "1";
 
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You won!");
-    } else if (computerScore > playerScore) {
-        console.log("You lost! Better luck next time!");
-    } else {
-        console.log("It ended in a draw!");
+        let gameResults = document.createElement("span");
+        document.body.appendChild(gameResults);
+        gameResults.style.cssText = "color: white; position: absolute; top: 25vh; right: 30vw;";
+        gameResults.style.fontSize = "80px";
+        gameResults.style.zIndex = "1";
+        gameResults.textContent = "Congratulations! You won!";
+
+        let playAgain = document.createElement("span");
+        document.body.appendChild(playAgain);
+        playAgain.textContent = "Play Again";
+        playAgain.style.cssText = "color: white; position: absolute; bottom: 30vh; right: 45vw;";
+        playAgain.style.fontSize = "50px";
+        playAgain.style.zIndex = "1";
+
+        playAgain.addEventListener("click", function() {
+            endScreen.remove();
+            gameResults.remove();
+            playerScore = 0;
+            computerScore = 0;
+            playerScreenScore.textContent = "0";
+            computerScreenScore.textContent = "0";
+            roundResults.textContent = "";
+            choice1.textContent = "";
+            choice2.textContent = "";
+            playAgain.remove();
+        });
+    } else if (computerScore === 5) {
+        let endScreen = document.createElement("div");
+        document.body.appendChild(endScreen);
+        endScreen.style.cssText = "position: absolute; top: 0; width: 100vw; height: 100vh;";
+        endScreen.style.backgroundColor = "black";
+        endScreen.style.zIndex = "1";
+        
+        let gameResults = document.createElement("span");
+        document.body.appendChild(gameResults);
+        gameResults.style.cssText = "color: white; position: absolute; top: 25vh; right: 26vw;";
+        gameResults.style.fontSize = "80px";
+        gameResults.style.zIndex = "1";
+        gameResults.textContent = "You lost! Better luck next time!";
+
+        let playAgain = document.createElement("span");
+        document.body.appendChild(playAgain);
+        playAgain.textContent = "Play Again";
+        playAgain.style.cssText = "color: white; position: absolute; bottom: 30vh; right: 45vw;";
+        playAgain.style.fontSize = "50px";
+        playAgain.style.zIndex = "1";
+
+        playAgain.addEventListener("click", function() {
+            endScreen.remove();
+            gameResults.remove();
+            playerScore = 0;
+            computerScore = 0;
+            playerScreenScore.textContent = "0";
+            computerScreenScore.textContent = "0";
+            roundResults.textContent = "";
+            choice1.textContent = "";
+            choice2.textContent = "";
+            playAgain.remove();
+        });
     }
+        }
 
-}
+btnRock.addEventListener("click", function() {
+    playerSelection = "Rock";
+    choice1.textContent = "Rock";
+    playRound();
+});
 
-game();    
+btnPaper.addEventListener("click", function() {
+    playerSelection = "Paper";
+    choice1.textContent = "Paper";
+    playRound();
+});
+
+btnScissors.addEventListener("click", function() {
+    playerSelection = "Scissors";
+    choice1.textContent = "Scissors";
+    playRound();
+});
